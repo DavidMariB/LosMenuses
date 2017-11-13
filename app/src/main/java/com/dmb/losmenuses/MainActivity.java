149571030008cae4adae4d9d20b2cb0f5dd50688
DavidMariB <davidmaribeltran@gmail.com>
2017-11-13 23:47:14 +0100

    Option Menu

--- app/src/main/java/com/dmb/losmenuses/MainActivity.java
+++ app/src/main/java/com/dmb/losmenuses/MainActivity.java
@@ -2,24 +2,37 @@ package com.dmb.losmenuses;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.PopupMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton img1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(myToolbar);
+
+        img1 = (ImageButton)findViewById(R.id.im1);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu){
+    public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.action_bar, menu);
+        inflater.inflate(R.menu.option_menu, menu);
         return true;
     }
+
+
+    public void showPopup(View v){
+        Toast t = Toast.makeText(getApplicationContext(),"Soy una Notificacion",Toast.LENGTH_LONG);
+        t.show();
+    }
+
+
 }
